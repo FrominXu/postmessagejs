1e7c14b0588dd076aed17b3bb7afe9e5eca6a11e
FrominXu <xushengfromin@qq.com>
2021-06-21 19:43:32 +0800

    chore: d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,10 @@
+/**
+ * window as server to be connected.
+ */
 interface ServerObject {
+  /**
+   * the server may be a frame.contentWindow、a new opened window、window.parent or window.opener
+   */
   server: Window;
   origin: string;
 }
@@ -11,8 +17,17 @@ declare namespace utils {
   function getOpenedServer(url: string): ServerObject;
 }
 interface Options {
+  /**
+   * timeout is set for client to connect with server, or for client and server's response of postMessage.then.
+   */
   timeout?: number;
+  /**
+   * eventFilter is filter for post messages event.
+   */
   eventFilter?: (e: any) => boolean;
+  /**
+   * called when connection destroyed.
+   */
   onDestroy?: (info: any) => void;
 }
 interface ServerOptions extends Options {
@@ -23,11 +38,28 @@ interface ClientOptions extends Options {
 }
 type PostMessagePayload = any;
 type PostMessageResponse = any;
+/**
+ * postMessage listener. method can be any customize event name.
+ */
 type PostMessageListener = (method: string, payload: PostMessagePayload, response: (response: PostMessageResponse) => void) => void;
 interface ConnectCallback {
+  /**
+   * post a message with a named event and payload data.
+   */
   postMessage: (method: string, payload: PostMessagePayload) => Promise<PostMessageResponse>;
   listenMessage: (listener: PostMessageListener) => void;
+  /**
+   * to destroy the connection
+   */
   destroy: () => void;
 }
+/**
+ * client use `callServer` to connect with server unless timeout. You can use the same `serverObject` to create more client-caller if necessary. (the server may be a frame.contentWindow、a new opened window、window.parent or window.opener)
+ * @param serverObject 
+ * @param options 
+ */
 export function callServer(serverObject: ServerObject, options: ClientOptions): Promise<ConnectCallback>;
+/**
+ * server use `startListening` to start a server listening, each server listening can only connect with one client. You can start more than one listening if necessary.
+ */
 export function startListening(options: ServerOptions): Promise<ConnectCallback>;
